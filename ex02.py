decf5e949276b4d186ac8a24183c3ff513342cb1
alpha.ﾠ <36175929+alphafofo@users.noreply.github.com>
2022-04-20 22:23:24 -0300

    Add files via upload

---
+++ ex02.py
@@ -0,0 +1,16 @@
+# TABUADA FEITA POR MIM
+
+escolha = int(input("Digite o numero da tabuada que você quer os resultados: "))
+
+print("============================")
+print(escolha, "x 1 =", escolha*1)
+print(escolha, "x 2 =", escolha*2)
+print(escolha, "x 3 =", escolha*3)
+print(escolha, "x 4 =", escolha*4)
+print(escolha, "x 5 =", escolha*5)
+print(escolha, "x 6 =", escolha*6)
+print(escolha, "x 7 =", escolha*7)
+print(escolha, "x 8 =", escolha*8)
+print(escolha, "x 9 =", escolha*9)
+print(escolha, "x 10 =", escolha*10)
+print("============================")
